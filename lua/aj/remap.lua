d58ab2c1653dc884a2d6ef911d78732438668361
server <billingsandrewjohn@gmail.com>
2024-08-23 12:57:50 -0400

    idek

--- lua/aj/remap.lua
+++ lua/aj/remap.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --vim.api.nvim_set_keymap('t', '<C-t>', [[<C-\><C-n>i dir\<CR>]], {noremap = true})
 vim.keymap.set("n", "{", "{zz", { noremap=true, silent=true })
 vim.keymap.set("n", "}", "}zz", { noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
--- lua/aj/set.lua
+++ lua/aj/set.lua
@@ -23,3 +23,6 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+--vim.opt.clipboard = "unnamedplus"
+
