eadd485c00ed70c82426535cfcdb9d2503dae159
server <billingsandrewjohn@gmail.com>
2024-08-24 15:32:39 -0400

    cleaned up packer

--- lua/aj/packer.lua
+++ lua/aj/packer.lua
@@ -3,9 +3,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
@@ -18,44 +16,21 @@ return require('packer').startup(function(use)
 		  vim.cmd('colorscheme rose-pine')
 	  end
   })
-
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
---  use {
---	  'VonHeikemen/lsp-zero.nvim',
---	  branch = 'v3.x',
---	  requires = {
--- 		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
--- 		  -- {'williamboman/mason.nvim'},
--- 		  -- {'williamboman/mason-lspconfig.nvim'},
--- 
--- 		  -- LSP Support
--- 		  {'neovim/nvim-lspconfig'},
--- 		  -- Autocompletion
--- 		  {'hrsh7th/nvim-cmp'},
--- 		  {'hrsh7th/cmp-nvim-lsp'},
--- 		  {'L3MON4D3/LuaSnip'},
 use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     requires = {
-        -- LSP Support
         {'neovim/nvim-lspconfig'},
-
-        -- Autocompletion
         {'hrsh7th/nvim-cmp'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'L3MON4D3/LuaSnip'}, -- Snippet engine and snippets collection
-
-        -- Optionally, to automatically install LSP servers
         {'williamboman/mason.nvim'},
         {'williamboman/mason-lspconfig.nvim'},
     }
 }
-
-  use ('github/copilot.vim')
---  use ('ojroques/vim-oscyank')
 end)
