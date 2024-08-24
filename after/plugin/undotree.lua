0c7ef19842463cf882de8ada401f30bbaceccf98
server <billingsandrewjohn@gmail.com>
2024-08-24 13:27:53 -0400

    added persistent undo history file to undotree.lua

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,2 +1,7 @@
-
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.opt.undofile = true
+local undodir = vim.fn.stdpath('data') .. '/undodir'
+vim.opt.undodir = undodir
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.mkdir(undodir, 'p')
+end
--- lua/aj/set.lua
+++ lua/aj/set.lua
@@ -7,19 +7,14 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
 --vim.opt.wrap = false
-
---vim.opt.hlsearch = false
+vim.opt.hlsearch = false
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 3
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
