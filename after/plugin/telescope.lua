023688d64d04773288105b735da12b5d53200921
Test User <test@example.com>
2025-04-09 22:18:26 -0400

    '

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,17 @@
+require('telescope').setup {
+    pickers = {
+        find_files = {
+            previewer = false
+        },
+        current_buffer_fuzzy_find = {
+            previewer = false
+        },
+        grep_string = {
+            previewer = false
+        }
+        -- Add other pickers as needed
+    }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 --vim.keymap.set('n', '<C-p>', builtin.git_files, {})
