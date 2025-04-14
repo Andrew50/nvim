d642450950a5f85dcbead1741be054140ef289ae
Test User <test@example.com>
2025-04-13 21:33:06 -0400

    set avante auto apply false

--- after/plugin/avante.lua
+++ after/plugin/avante.lua
@@ -14,7 +14,7 @@ require('avante').setup({
     auto_suggestions = false,
     auto_set_highlight_group = true,
     auto_set_keymaps = true,
-    auto_apply_diff_after_generation = true,
+    auto_apply_diff_after_generation = false,
     support_paste_from_clipboard = true,
     --highlight_input_on_submit = true,
   },
